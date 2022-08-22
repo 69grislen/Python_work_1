dfc0330e4bae0c189dc72ceaca989c0835521518
69grislen <108180181+69grislen@users.noreply.github.com>
2022-08-22 20:27:57 +0500

    Add files via upload

---
+++ python_work/4_1 , 4_2.py
@@ -0,0 +1,19 @@
+#4_1
+pizzas = ["margaret", "four cheeses", "hawaiian"]
+for pizza in pizzas:
+    print(f"Эта пицца называется {pizza}")
+    print(f"Я обажаю {pizza}")
+print("Я реально обажаю пиццу!")
+
+#4_2
+animals = ["dog", "cat", "bear"]
+for animal in animals:
+    print(f"Это достаточно милое животное называется - {animal}")
+print("Any of these animals would make a great pet!")
+
+
+
+
+
+
+
